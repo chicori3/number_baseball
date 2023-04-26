92fcceb01a8cfc171b9390434c16df368b41c0c7
sian <chicori3@naver.com>
2023-04-26 16:38:38 +0900

    feat(BallGeneratorTest): 테스트 클래스를 프로덕션으로 이동

--- src/test/java/org/baseball/domain/BallsGeneratorTest.java
+++ src/test/java/org/baseball/domain/BallsGeneratorTest.java
@@ -1,4 +1,4 @@
-package org.baseball;
+package org.baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,18 +7,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.security.SecureRandom;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-public class BallGeneratorTest {
+public class BallsGeneratorTest {
 
     private BallsGenerator ballsGenerator;
 
@@ -77,57 +73,5 @@ public class BallGeneratorTest {
     }
 }
 
-class BallsGenerator {
-    public List<Integer> randomBallsGenerate() {
-        SecureRandom random = new SecureRandom();
-        return IntStream.generate(() -> random.nextInt(9) + 1)
-                .distinct()
-                .limit(3)
-                .boxed()
-                .collect(Collectors.toList());
-    }
 
-    public List<Integer> customBallsGenerate(int... numbers) {
-        isValid(numbers);
-
-        return IntStream.of(numbers)
-                .boxed()
-                .collect(Collectors.toList());
-    }
-
-    private void isValid(int[] numbers) {
-        isValidLength(numbers);
-        isValidNumber(numbers);
-        hasDuplicatedNumber(numbers);
-    }
-
-    private void isValidNumber(int[] numbers) {
-        IntStream.of(numbers)
-                .filter(number -> number < 1 || number > 9)
-                .findAny()
-                .ifPresent((number) -> {
-                    throw new IllegalArgumentException("1~9 사이의 숫자를 입력해주세요.");
-                });
-    }
-
-    private void hasDuplicatedNumber(int[] numbers) {
-        Set<Integer> numbersSet = convertArrToSet(numbers);
-
-        if (numbers.length < numbersSet.size()) {
-            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
-        }
-    }
-
-    private void isValidLength(int[] numbers) {
-        if (numbers.length != 3) {
-            throw new IllegalArgumentException("3개의 숫자를 입력해주세요.");
-        }
-    }
-
-    private Set<Integer> convertArrToSet(int[] numbers) {
-        return Arrays.stream(numbers)
-                .boxed()
-                .collect(Collectors.toSet());
-    }
-}
 
